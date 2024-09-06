d27914010ff7a535bacf101c7c951ac732ea0304
David Jefts <david.jefts@fireflyspace.com>
2024-09-06 15:42:47 -0500

    Create Batch script to generate symlinks for program config files

---
+++ windows_setup.bat
@@ -0,0 +1,4 @@
+#!/bin/sh & :: this allows Git Bash to tab-complete
+mklink /j %userprofile%\.minttyrc %userprofile%\setup-files\.minttyrc
+mklink /j %userprofile%\.vimrc %userprofile%\setup-files\.vimrc
+mklink /j %userprofile%\.wslconfig %userprofile%\setup-files\.wslconfig
