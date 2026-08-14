bfa1ed534c777fcc240ed2acd3a8539117f320e8
David Jefts <david.jefts@fireflyspace.com>
2026-08-14 13:58:24 -0500

    feat: better docker ps command

--- bash_commands/docker.sh
+++ bash_commands/docker.sh
@@ -23,8 +23,38 @@ docker() {
         # docker enter [container]
         command docker exec -it "$2" sh
     elif [[ "$1" = "${commands[4]}" ]]; then
-        # docker ps overwrite for cleaner output
-        command docker ps --format "table {{.ID}}\t{{.Names}}\t{{.CreatedAt}}\t{{.Status}}\t{{.Ports}}"
+        # docker ps overwrite for cleaner output.
+        # docker ps templates only expose the network NAME (e.g. glow_default),
+        # not the host bridge INTERFACE (e.g. br-fireflydev) that firewall rules
+        # target. Build a name->bridge map from `docker network inspect` and add
+        # a BRIDGE column. Extra args after `ps` (e.g. -a) are passed through.
+        shift
+        {
+            printf 'CONTAINER ID\tNAMES\tSTATUS\tPORTS\tNETWORKS\tBRIDGE\n'
+            local _id _name _br _cid _names _status _ports _nets _out _n
+            declare -A _bridge
+            while IFS='|' read -r _id _name; do
+                _br=$(command docker network inspect "$_id" \
+                    --format '{{index .Options "com.docker.network.bridge.name"}}' 2>/dev/null)
+                [[ -z $_br ]] && _br="br-${_id:0:12}"
+                _bridge[$_name]=$_br
+            done < <(command docker network ls --format '{{.ID}}|{{.Name}}')
+
+            command docker ps "$@" \
+                --format '{{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}\t{{.Networks}}' \
+            | while IFS=$'\t' read -r _cid _names _status _ports _nets; do
+                _out=""
+                local _arr; IFS=',' read -ra _arr <<< "$_nets"
+                for _n in "${_arr[@]}"; do
+                    _n="${_n# }"; _n="${_n% }"
+                    _out="${_out:+$_out,}${_bridge[$_n]:-$_n}"
+                done
+                [[ -z $_ports ]] && _ports='-'
+                [[ -z $_nets ]] && { _nets='-'; _out='-'; }
+                printf '%s\t%s\t%s\t%s\t%s\t%s\n' \
+                    "$_cid" "$_names" "$_status" "$_ports" "$_nets" "$_out"
+            done
+        } | column -t -s $'\t'
     elif [[ "$1" = "${commands[5]}" ]]; then
         # docker
         command docker
